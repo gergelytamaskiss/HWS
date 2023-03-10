465cd41a020c72e4bdb3566c3f6745bccf040ef2
Gergely Kiss <gergelytamas.kiss@gmail.com>
2023-03-10 22:32:33 +0100

    Add GuessTheFlag app

---
+++ SwiftUI/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gergely Kiss on 2023. 03. 07..
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
